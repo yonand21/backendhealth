4f8343c0dfe113363cef1d4dc4442b43eec8ab8b
Yonanda Putra <93269134+yonand21@users.noreply.github.com>
2023-06-20 00:41:48 +0800

    Update artikel.js

--- src/artikel.js
+++ src/artikel.js
@@ -5,7 +5,7 @@ const artikel = [
     'slug': '5-olahraga-untuk-ibu-hamil',
     'category': 'Kehamilan',
     'img': 'https://lh6.googleusercontent.com/LR3IsdrZxLkDVR5a1pt2s5w6m-PPSPYCqsDayGU1BjDkZyeb0FMYEg4fM9_5TaTicVw=w2400',
-    'source': 'halodoc.com',
+    'source': 'https://id.theasianparent.com/olahraga-untuk-ibu-hamil-muda',
     'overview': 'Halodoc. “Ibu hamil juga perlu melakukan olahraga agar kesehatan tetap terjaga dengan baik. Ada berbagai jenis olahraga untuk ibu hamil, seperti be...',
   },
   {
@@ -14,7 +14,7 @@ const artikel = [
     'slug': 'Tips-Olahraga-Aman-saat-Puasa,-Mending-Habis-Sahur-atau-Sebelum-Buka?',
     'category': 'Kebugaran',
     'img': 'https://akcdn.detik.net.id/community/media/visual/2023/03/22/1268399964_169.jpeg?w=700&q=90',
-    'source': 'health.detik.com',
+    'source': 'https://health.detik.com/kebugaran/d-6632725/tips-olahraga-aman-saat-puasa-mending-habis-sahur-atau-sebelum-buka',
     'overview': 'Detikhealth. Puasa tidak seharusnya menjadi alasan untuk malas berolahraga. Pasalnya, be...',
   },
   {
@@ -23,7 +23,7 @@ const artikel = [
     'slug': 'Yuk-Tetap-Sehat-Bebas-Anemia',
     'category': 'Umum',
     'img': 'https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2021/06/16070755/Anemia.jpg',
-    'source': 'health.detik.com',
+    'source': 'https://www.biofarma.co.id/id/announcement/detail/yuk-tetap-sehat-bebas-anemia-',
     'overview': 'Bio Farma. Anemia adalah kondisi ketika tubuh kekurangan sel darah merah...',
   },
   {
@@ -32,7 +32,7 @@ const artikel = [
     'slug': '12-Manfaat-Bersepeda-untuk-Kesehatan-Bisa-Mengurangi-Stres-Lho',
     'category': 'Kebugaran',
     'img': 'https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2021/10/18011104/Bukan-Cuma-Menyehatkan-Jantung-Ini-X-Manfaat-Bersepeda-01.jpg',
-    'source': 'health.detik.com',
+    'source': 'https://health.detik.com/kebugaran/d-6757871/12-manfaat-bersepeda-untuk-kesehatan-bisa-mengurangi-stres-lho',
     'overview': 'Detikhealth. Bersepeda adalah aktivitas yang menyenangkan dan memiliki banyak...',
   },
   {
@@ -41,7 +41,7 @@ const artikel = [
     'slug': 'Cukup-30-Menit-Sehari-Lari-Pagi-Bisa-Sehatkan-Otot-Tulang-Jantung',
     'category': 'Kebugaran',
     'img': 'https://cdnwpedutorenews.gramedia.net/wp-content/uploads/2021/03/02135350/pengertian-lari-pendek-min-810x555.jpg',
-    'source': 'health.detik.com',
+    'source': 'https://health.detik.com/kebugaran/d-6704736/cukup-30-menit-sehari-lari-pagi-bisa-sehatkan-otot-tulang-jantung',
     'overview': 'Detikhealth. Lari pagi adalah kegiatan yang bermanfaat untuk kesehatan tubuh. Dengan...',
   },
   {
@@ -50,7 +50,7 @@ const artikel = [
     'slug': 'Kompres-untuk-Demam-dengan-Air-Panas-atau-Dingin-Efektif',
     'category': 'Kebugaran',
     'img': 'https://asset.kompas.com/crops/PDA8rdw8kPepotoZWez_4YylksQ=/0x0:1000x667/750x500/data/photo/2022/02/25/6218fc0c9185c.jpg',
-    'source': 'health.detik.com',
+    'source': 'https://health.detik.com/kebugaran/d-6704736/cukup-30-menit-sehari-lari-pagi-bisa-sehatkan-otot-tulang-jantung',
     'overview': 'Detikhealth. Ketika demam, kompres dapat membantu mengurangi suhu tubuh. Namun, pertanyaan yang...',
   },
   {
@@ -59,7 +59,7 @@ const artikel = [
     'slug': 'Inilah-4-Skill-Pertolongan-Pertama-yang-Wajib-Dikuasai',
     'category': 'Kebugaran',
     'img': 'https://www.pelatihansertifikasi.com/image/2018-10-first-aid-660x405.jpg',
-    'source': 'health.detik.com',
+    'source': 'https://health.detik.com/kebugaran/d-6704736/cukup-30-menit-sehari-lari-pagi-bisa-sehatkan-otot-tulang-jantung',
     'overview': 'Detikhealth. Kejadian darurat bisa terjadi kapan saja dan di mana saja. Oleh karena...',
   },
   {
@@ -68,7 +68,7 @@ const artikel = [
     'slug': 'Morning-Sickness-Adalah-Penyebab-Gejala-dan-Cara-Mengatasinya',
     'category': 'Kebugaran',
     'img': 'https://www.arabnews.com/sites/default/files/styles/n_670_395/public/2019/10/02/1779216-1902343925.jpg?itok=tKLQK_X8',
-    'source': 'health.detik.com',
+    'source': 'https://health.detik.com/kebugaran/d-6704736/cukup-30-menit-sehari-lari-pagi-bisa-sehatkan-otot-tulang-jantung',
     'overview': 'Detikhealth. Morning sickness atau mual-mual di pagi hari adalah gejala umum...',
   },
 ];
